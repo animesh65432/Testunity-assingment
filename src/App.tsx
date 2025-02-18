e4596f9e5ee548cfce3fbf240d44c977c0a0ccd8
animesh65432 <kiranduttta234@gmail.com>
2025-02-18 18:55:04 +0530

    intial setup for project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const App: React.FC = () => {
+  return (
+    <div className='flex h-dvh justify-center items-center'>
+      <div>
+        <Button>Click Me</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
